3d00f5869581ccbe52f3d1a46e570d80f8eabc9c
Paula Ortiz <sarkofy@hotmail.com>
2019-02-19 16:18:45 -0600

    Ejemplo de conversión explícita

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+
+public class Conversion
+{
+    public static void main(String[] args)
+    {
+      double calif = 9.8763; 
+           
+   int l = (int) calif;
+        System.out.println("Valor int "+l); 
+         
+    } 
+} 
