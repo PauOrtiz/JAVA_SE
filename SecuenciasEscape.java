b615fa99286d71c1baf22817d694bca0b5d73dac
Paula Ortiz <sarkofy@hotmail.com>
2019-02-21 10:47:23 -0600

    Ejemplo secuencias de escape

---
+++ SecuenciasEscape.java
@@ -0,0 +1,8 @@
+public class SecuenciasEscape {
+     public static void main(String args[]) {
+         String saludo="Hola, bienvenido al curso de fundamentos de JAVA SE ";
+String mensaje="Cualquier duda pregúntame";
+System.out.println(saludo +"\n" +mensaje);
+
+     }
+ }
