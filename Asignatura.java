497dcb559b35f3969d87e037bbb83072317c0c94
Paula Ortiz <sarkofy@hotmail.com>
2019-03-07 16:47:52 -0600

    cambio en tipo de datos de los parámetros del constructor

--- Asignatura.java
+++ Asignatura.java
@@ -1,17 +1,19 @@
+package geometría;
+
 public class Asignatura{
 String nombreAsignatura;
-int claveAsignatura;
+String claveAsignatura;
 String nombreProfesor;
 String diaImparticion;
 
-public Asignatura(String Asignatura,int clave,String Profesor,String horario){ //EN LOS PAREMTRSO INDICAS LO QUE VAS A INICIALIZAR
+public Asignatura(String Asignatura,String clave,String Profesor,String horario){ //EN LOS PAREMTRSO INDICAS LO QUE VAS A INICIALIZAR
 nombreAsignatura=Asignatura; //EMPATAS LAS VARIABLES QUE TIENES CON LO QUE SE ESPERA QUE RECIBA
 claveAsignatura=clave;
 nombreProfesor=Profesor;
-diaImparticion=horario
+diaImparticion=horario;
 }
 public static void main(String[]args){
-Asignatura miAsignatura=new Asignatura("Matemáticas",890,"Aldo Martínez", "Jueves");
-System.out.println(miAsignatura.asignatura+","miAsignatura.clave+","+miAsignatura.nombreProfesor+","+ miAsignatura.diaImparticion);
+Asignatura miAsignatura=new Asignatura("Matemáticas","AS3","Aldo Martínez", "Jueves");
+System.out.println(miAsignatura.nombreAsignatura+","+miAsignatura.claveAsignatura +","+miAsignatura.nombreProfesor+","+ miAsignatura.diaImparticion);
    }
 }
