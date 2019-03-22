22ce14af48b3805ec699ac37f88b54c83c1b37a5
Paula Ortiz <sarkofy@hotmail.com>
2019-03-22 15:03:32 -0600

    Métodos para arreglos

--- arreglos.java
+++ arreglos.java
@@ -1,15 +1,25 @@
-package cadenasArreglos; 
+package cadenasArreglos;
+import java.util.Arrays;
 
 public class arreglos {
 	private int[] num= {1456,2019,3289,5678,9998,1021,3670,3089,2768,8976};
-    public void recorrerArreglo(){
+	public void recorrerArreglo(){
     	System.out.println("Los valores que contiene el arreglo son:\n");
      for(int i=0; i<10;i++) {
     	System.out.println("En el índice "+i+":\n"+ num[i]);
      }
     }
+    public void ordenaArreglo() {
+     Arrays.sort(num);//ordena de forma ascendente
+     System.out.println("Los elementos del arreglo en orden son:\n");
+     for(int i=0; i<10;i++) {
+    	System.out.println(num[i]+"   "+ "En el índice  "+i);
+      }
+    }
+     
 	public static void main(String[] args) {
 		arreglos miArreglo=new arreglos();
 		miArreglo.recorrerArreglo();
+		miArreglo.ordenaArreglo();	
 	}
 }
