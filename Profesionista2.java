43495825d647196e518156120698a665b227e021
Paula Ortiz <sarkofy@hotmail.com>
2019-04-04 15:50:36 -0600

    Redefinición e invocación de método sobreescrito y original

--- Profesionista2.java
+++ Profesionista2.java
@@ -1,4 +1,5 @@
 package herencia_polimorfismo;
+package herencia_polimorfismo;
 
 public class Profesionista2 extends Persona{
 
@@ -7,9 +8,15 @@ public class Profesionista2 extends Persona{
     	return numAfiliacion;
     	
     }
+	//usando el método del padre
+	public void original() {
+	 System.out.println(super.getNumAfiliacion());	
+	}
 	public static void main(String[] args) {
 		Profesionista2 nuevoP=new Profesionista2();
+		Profesionista2 segundoNuevoP=new Profesionista2();
 		nuevoP.setAfiliacion(67453);
-		System.out.println(nuevoP.getNumAfiliacion());
+		segundoNuevoP.setAfiliacion(12345);// ejecuta el método del padre
+		segundoNuevoP.original();
 	}
 }
