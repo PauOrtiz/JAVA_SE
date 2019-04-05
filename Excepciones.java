e6808860f028c65f3f888d3443281b45b7e97db3
Paula Ortiz <sarkofy@hotmail.com>
2019-04-05 12:03:20 -0600

    Bloque try...catch...finally

--- Excepciones.java
+++ Excepciones.java
@@ -1,3 +1,5 @@
+package excepctionJava;
+
 public class CadenaArreglo extends arreglos {
  private String cadena="Manejo de excepciones en Java";
  private char[] arr= cadena.toCharArray();
@@ -8,6 +10,15 @@ public class CadenaArreglo extends arreglos {
  	for(int i=0; i<arr.length;i++) {
  	System.out.println(arr[i]);
       }
+ 	try { //el bloque conflictivo
+ 	System.out.println("El contenido de arr[-10] es:"+ arr[-10]); //no existen índices negativos por lo que se produce una excepción del tipo java.lang.ArrayIndexOutOfBoundsException
+ 	  } //cierre
+ 	catch(ArrayIndexOutOfBoundsException excepcion)
+ 	{
+ 		 System.out.println(" Imposible acceder");
+ 	}finally {
+ 		System.out.println("Bloque try...catch finalizado");
+ 	}
  }
   	public static void main(String[] args) {
 		CadenaArreglo nuevoArreglo=new CadenaArreglo();
