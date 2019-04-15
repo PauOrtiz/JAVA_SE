2c4097440a798b2ebca374d20799813fae626f26
Paula Ortiz <sarkofy@hotmail.com>
2019-04-15 14:58:18 -0500

    Primer Jframe

---
+++ ElementosSwing.java
@@ -0,0 +1,11 @@
+import javax.swing.*; 
+
+public class ElementosSwing extends JFrame { //jframe ventana principal de la aplicación y solo debe ser una
+
+	public static void main(String[] args) {
+		ElementosSwing ventana=new ElementosSwing(); 
+		ventana.getContentPane();
+		ventana.setSize(400,400);
+		ventana.setVisible(true);
+	}
+}
