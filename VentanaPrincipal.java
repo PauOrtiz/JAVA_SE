ae271540bb390a7304bfe10541f8f356e313a1cb
Paula Ortiz <sarkofy@hotmail.com>
2019-04-23 16:33:45 -0500

    JFrame,JLabel,JButton,Listener

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -2,34 +2,45 @@ package interfacesgraficas;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
+import java.awt.Color;
+/**
+ * Una clase que dibuja una ventana con un botón, el cual al ser presionado por el usuario
+ * lanza una ventana secundaria del tipo JDialog modal
+ * @version 1.0
+ * @author CPE
+ * */
+/**clase VentanaPrincipal hereda de JFrame*/
 public class VentanaPrincipal extends JFrame implements ActionListener{
-  
-private JButton b;
-private JLabel etiqueta;
-    
-	
+	private JButton b;
+	private JLabel etiqueta;
+/**Inicialización de los componentes gráficos en constructor*/
 	public VentanaPrincipal() {
-		setLayout(null);
+		setLayout(null); /*Componentes en distribución personalizada*/
+/**Etiqueta*/
 		etiqueta = new JLabel("Registro veterinario Happy pet v1.0");
 		etiqueta.setBounds(115,60,200,30);
+		etiqueta.setHorizontalAlignment(JLabel.CENTER);
 		add(etiqueta);
+/**Botón*/	
 		b = new JButton("Accede");
 		b.setBounds(105,110,200,40);
+		b.setBackground(Color.GREEN);
 		add(b);
-		b.addActionListener(this);   
+		b.addActionListener(this);     
 	}
-    
- public void actionPerformed(ActionEvent e) {
+/**Listener*/
+	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == b) {
 			VentanaSecundaria s = new VentanaSecundaria(this,true);
 			s.setVisible(true);
 		}
 	}
-
+/**Método principal*/
 	public static void main(String[] args) {
 		VentanaPrincipal v=new VentanaPrincipal();
-		v.setBounds(10,10,410,210);
+		v.setSize(400,300);
+		v.setResizable(false); 
+		v.setLocationRelativeTo(null);
 		v.setVisible(true);
 	}
 }
