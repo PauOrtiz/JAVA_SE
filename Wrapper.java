0b7ed7ae109126804de85c7f2c0c97210e031688
Paula Ortiz <sarkofy@hotmail.com>
2019-04-26 15:03:12 -0500

    Wrapper

---
+++ Wrapper.java
@@ -0,0 +1,10 @@
+public class Wrapper { 
+  public static void main(String[] args) { 
+    Integer i= 345; 
+    Double d= 98.99; 
+    Character c = 'c'; 
+    System.out.println(i);
+    System.out.println(d);
+    System.out.println(c);
+  }
+}
