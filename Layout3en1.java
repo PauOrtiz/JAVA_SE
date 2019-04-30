9ba2c88137c680d461e76e789f53aa553371113d
Paula Ortiz <sarkofy@hotmail.com>
2019-04-30 16:26:36 -0500

    Happy Pet, cambio en interfaz gráfica

--- Layout3en1.java
+++ Layout3en1.java
@@ -1,4 +1,4 @@
-package interfacesgraficas;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
@@ -16,12 +16,13 @@ import javax.swing.WindowConstants;
 
 public class Layout3en1 {
     //variables y comtenedores
-    private JLabel titulo, etiqueta;
+    private JLabel titulo, etiqueta,etiqueta1,etiqueta2,etiqueta3;
     private JPanel panelIzquierdo, panelDerecho, panelInferior,panelCentral;
     private JButton botonIz1, botonIz2, botonIz3;
     private JButton botonDe1, botonDe2, botonDe3;
     private JButton    botonIn1, botonIn2;
     private JPanel datos;
+    private JTextField textoCampo,textoCampo2,textoCampo3,textoCampo4;
 
     public void contruyePanelIzquierdo(){
         panelIzquierdo = new JPanel();
@@ -36,33 +37,35 @@ public class Layout3en1 {
     }
     public void construyePanelCentral() {
     	 datos = new JPanel();
-    	 etiqueta=new JLabel("Datos de la mascota");
-    	 //centrar una etiueta
+    	 etiqueta=new JLabel("Nombre de la mascota");
+    	 datos.add(etiqueta);
+    	 textoCampo=new JTextField("Nombre",50);
+    	 datos.add(textoCampo);
+    	 etiqueta1=new JLabel("Nombre del dueño");
+    	 datos.add(etiqueta1);
+    	 textoCampo2=new JTextField("Nombre1",50);
+    	 datos.add(textoCampo2);
+    	 etiqueta2=new JLabel("Apellido del dueño");
+    	 datos.add(etiqueta2);
+    	 textoCampo3=new JTextField("Apellido",50);
+    	 datos.add(textoCampo3);
+    	 etiqueta3=new JLabel("Correo electrónico");
+    	 datos.add(etiqueta3);
+    	 textoCampo4=new JTextField("Mail",50);
+    	 datos.add(textoCampo4);
+    	 //lograr que el campo de texto aparezca al lado de label
     	
     	 datos.setLayout(new BoxLayout(datos,BoxLayout.Y_AXIS));
          datos.setBackground(Color.blue);
-         datos.add(etiqueta);
+         
     }
     
-
-    public void contruyePanelDerecho(){
-        panelDerecho = new JPanel();
-        botonDe1=new JButton("Der1");
-        botonDe2=new JButton("Der2");
-        botonDe3=new JButton("Der3");
-        panelDerecho.setLayout(new BoxLayout(panelDerecho,BoxLayout.Y_AXIS));
-        panelDerecho.setBackground(Color.blue);
-        panelDerecho.add(botonDe1);
-        panelDerecho.add(botonDe2);
-        panelDerecho.add(botonDe3);
-    }
-
     public void contruyePanelInferior(){
         panelInferior = new JPanel();
-        botonIn1=new JButton("Aceptar");
+        botonIn1=new JButton("Guardar");
         botonIn2=new JButton("Cancelar");
         panelInferior.setLayout(new FlowLayout());
-        panelInferior.setBackground(Color.green);
+        panelInferior.setBackground(Color.black);
         panelInferior.add(botonIn1);
         panelInferior.add(botonIn2);
     }
@@ -73,25 +76,29 @@ public class Layout3en1 {
         Font aux=titulo.getFont();
         titulo.setFont(new Font(aux.getFontName(), aux.getStyle(), 20));
         frame.setLayout(new BorderLayout());
-
+     
         //agregamos los paneles al frame principal
         frame.add(titulo,BorderLayout.NORTH);//centro veterinario
         frame.add(datos, BorderLayout.CENTER);//datos
         frame.add(panelIzquierdo,BorderLayout.WEST);//foto
-        frame.add(panelDerecho,BorderLayout.EAST);//se anula
+        //frame.add(panelDerecho,BorderLayout.EAST);//se anula
         frame.add(panelInferior,BorderLayout.SOUTH);//footer
         //Configuramos el frame
         frame.pack();
         frame.setVisible(true);
-       
+        frame.setSize(400,300);
+		//frame.setResizable(false); 
+		frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
     }
 
     public Layout3en1(){
         contruyePanelInferior();
         contruyePanelIzquierdo();
-        contruyePanelDerecho();
+        //contruyePanelDerecho();
         construyePanelCentral();
+       
         contruyeVentana();
     }
 
