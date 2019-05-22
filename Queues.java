1ab78c596f78ab687fb70120ce578ef0f4d2e999
Paula Ortiz <sarkofy@hotmail.com>
2019-05-22 16:27:56 -0500

    Método operaciones

--- Queues.java
+++ Queues.java
@@ -1,22 +1,25 @@
 import java.util.*;
   
-public class Queues 
-{ 
+  public class Queues 
+{ 	
+   public  void operaciones() {
+	   Queue<Integer> cola = new LinkedList<>(); 
+	   for (int i=0; i<10; i++) 
+		    cola.add(i); 
+		    System.out.println("Elementos:"+cola); 
+		    int remuevePrincipio = cola.remove(); 
+		    System.out.println("Elemento removido:" + remuevePrincipio);  
+		    int h = cola.peek(); 
+		    System.out.println("El primer elemento de la cola es:" + h); 
+		  
+		    /* Otros métodos a implementar
+		    ...*/
+		    int n = cola.size(); 
+		    System.out.println("Tamaño de la estructura:" + n); 
+ }
   public static void main(String[] args) 
   { 
-    Queue<Integer> cola = new LinkedList<>(); 
-    for (int i=0; i<10; i++) 
-    cola.add(i); 
-    System.out.println("Elementos"+cola); 
-    int remuevePrincipio = cola.remove(); 
-    System.out.println("Elemento removido" + remuevePrincipio); 
-    System.out.println(remuevePrincipio); 
-    int h = cola.peek(); 
-    System.out.println("El primer elemento de la cola es" + h); 
-  
-    /* Otros métodos a implementar
-    ...*/
-    int n = cola.size(); 
-    System.out.println("Tamaño de la estructura" + n); 
+    Queues opColas=new Queues();
+    opColas.Operaciones();   
   } 
 } 
