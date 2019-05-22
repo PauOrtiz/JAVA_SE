9c68e5ba3ad6fa26a6cfd608ff42c718a8940662
Paula Ortiz <sarkofy@hotmail.com>
2019-05-22 16:39:46 -0500

    Método desencolar

--- Queues.java
+++ Queues.java
@@ -1,9 +1,13 @@
 import java.util.*;
   
-  public class Queues 
-{ 	
+  import java.util.*;
+  
+public class Queues 
+{ 
+	private int n;
+	 Queue<Integer> cola = new LinkedList<>(); 
    public  void operaciones() {
-	   Queue<Integer> cola = new LinkedList<>(); 
+	  
 	   for (int i=0; i<10; i++) 
 		    cola.add(i); 
 		    System.out.println("Elementos:"+cola); 
@@ -14,12 +18,18 @@ import java.util.*;
 		  
 		    /* Otros métodos a implementar
 		    ...*/
-		    int n = cola.size(); 
+		    n = cola.size(); 
 		    System.out.println("Tamaño de la estructura:" + n); 
- }
+     }
+   public void desencolar() {
+	   while(cola.poll()!=null) {
+		System.out.println("El nuevo frente de la cola es:"+cola.peek());
+	    }
+    }
   public static void main(String[] args) 
   { 
     Queues opColas=new Queues();
-    opColas.Operaciones();   
+    opColas.operaciones();
+    opColas.desencolar(); 
   } 
 } 
