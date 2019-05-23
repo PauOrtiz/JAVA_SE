a77f9a2b1e80b3f9ea4a069eb277ecbfbf04ada9
Paula Ortiz <sarkofy@hotmail.com>
2019-05-23 16:06:29 -0500

    setters y getters para consulta js a partir de java

--- Prueba.java
+++ Prueba.java
@@ -4,24 +4,28 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-
 public class Prueba {
-	
-	public void setNombre(String s){
-		
-}
+	private static String Nombre;
+    public Prueba(String Nombre){ //constructor
+       Nombre="";
+    }	
+	public void setNombre(String Nombre){ 
+		Prueba.Nombre=Nombre;
+   }
+   public String getNombre(){
+    return Prueba.Nombre;
+  }
 public static void main(String args[]) {
 ScriptEngineManager manager= new ScriptEngineManager();
 ScriptEngine motor= manager.getEngineByName("nashorn");
 try {
-Object resultado= motor.eval("print('hola')");
-Prueba p= new Prueba();
-p.setNombre("maria");
+Object resultado= motor.eval("print('BIENVENIDO AL:')");
+Prueba p= new Prueba(Nombre);
+p.setNombre("Sistema de control veterinario");
 motor.put("persona", p);
 motor.eval("print(persona.getNombre())");
-} catch (ScriptException e) {
-// TODO Auto-generated catch block
+  } catch (ScriptException e) {
 e.printStackTrace();
-}
-}
+  }
+ }
 }
