2edcf0b4ca5038064e9e9364e1abcb2ddbb30826
Paula Ortiz <sarkofy@hotmail.com>
2019-05-24 15:49:13 -0500

    Ejecuta método java definido previamente

---
+++ javaScriptF2.js
@@ -0,0 +1,5 @@
+var suma=function(input1,input2){
+	/*paquete.script.nombredelarchivojs*/
+	var m=Java.type("js.MetodoJavaEnJs")//creas al objeto java
+	print("La suma de ambos es:"+m.suma(input1,input2)) //suma es el método java
+}
