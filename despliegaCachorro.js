62111203183f20ba5f1117facb2343e1f24404cc
Paula Ortiz <sarkofy@hotmail.com>
2019-05-29 10:31:38 -0500

    función que meustra en orden el nombre de los cachorros

---
+++ despliegaCachorro.js
@@ -0,0 +1,7 @@
+var mascotas = ["Michelle", "Coco", "Victoria", "Camila"];
+print("Nuestros cachorros se llaman:"+ mascotas);
+function despliegaCachorros() { 
+var mascotas2=mascotas.sort();
+ print("Su nombre en orden alfabético:"+ mascotas2);
+}
+despliegaCachorros();
