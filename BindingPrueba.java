ed768c2210b906e150fa13b487b3af57adf3100f
Paula Ortiz <sarkofy@hotmail.com>
2019-05-30 16:31:48 -0500

    Bindings, get y remove

--- BindingPrueba.java
+++ BindingPrueba.java
@@ -7,8 +7,15 @@ public class BindingPrueba { //Binding enlace de clases y métodos de forma clav
 	public static void main(String[] args) {
 		Bindings p= new SimpleBindings();
 		p.put("cachorra", "michelle");
+        p.put("raza", "mix de pastor belga");
         //Extracción del contenido
 		Object cachorra=p.get("cachorra");
 		System.out.println("cachorra=" + cachorra);
+		Object raza=p.get("raza");
+        System.out.println("raza=" + raza);
+        //Removiendo
+        p.remove("cachorra");
+        cachorra=p.get("cachorra");
+        System.out.println("Nombre de la cachorra:"+ cachorra);
 	}
 }
