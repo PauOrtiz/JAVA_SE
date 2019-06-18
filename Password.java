3ed0ed5e64afc8dfbacf176e30cd6bbf31df2018
Paula Ortiz <sarkofy@hotmail.com>
2019-06-18 16:48:21 -0500

    Métodos para obtener contraseña y su longitud

--- Password.java
+++ Password.java
@@ -1,13 +1,14 @@
 package ejercicios;
-import java.util.Scanner; //https://www.discoduroderoer.es/ejercicios-propuestos-y-resueltos-programacion-orientado-a-objetos-java/
+import java.util.Scanner; 
 
 public class Password {
   private int longitud;
   private String contraseña;
   private static Scanner obDatos, obDatos2;
   
-   public Password(int l) {
+   public Password(int l,String c) {
 	 this.longitud=l; 
+	 this.contraseña=c;
    }
    
    public void setContraseña(String c) {
@@ -22,20 +23,31 @@ public class Password {
    public int getLongitud() {
 	   return longitud;
    }
-   
+   public void leePassword() {
+	   
+   }
+   public void obtenerContraseña() {
+	   System.out.println("Ingrese la contraseña:");
+	   contraseña=obDatos.nextLine();
+   }
+   public void obtenerLongitud() {
+	   System.out.println("Ingresa la longitud:");
+	   longitud=obDatos2.nextInt();
+   }
    public boolean esFuerte(String c) {
 	   boolean fuerte=false;
 	   if(c>5) {//establecer bien la condición
 		 fuerte=false;  
 	   }
-	    return fuerte;
+    return fuerte;
    }
    
-   public void generarPassword() {
+   public void generarPassword(int l) {
+	   //genera de forma aleatoria una contraseña
 	   
    }
 	public static void main(String[] args) {
-		int arreglo[]=;
+		int passwords[]=; //crear el array de passwords y rellenarlo con los datos de getters
 		Password p=new Password();
 		obDatos = new Scanner(System.in);
 		obDatos2 = new Scanner(System.in);
