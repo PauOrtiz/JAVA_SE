9e99d9edb522a48106fa74e557626b2694581500
Paula Ortiz <sarkofy@hotmail.com>
2019-06-18 15:50:56 -0500

    Validación de password

---
+++ Password.java
@@ -0,0 +1,12 @@
+
+public class Password {
+ private int longitud;
+ private String contraseña;
+  public Password(int l) {
+	 this.longitud=l; 
+  }
+	public static void main(String[] args) {
+		Password p=new Password();
+	}
+
+}
