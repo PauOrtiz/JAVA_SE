984ec7ae9f6964e633baee40170f86d024156abc
Paula Ortiz <sarkofy@hotmail.com>
2019-06-18 16:00:10 -0500

    Esqueleto de métodos

--- Password.java
+++ Password.java
@@ -1,12 +1,34 @@
 
 public class Password {
- private int longitud;
- private String contraseña;
-  public Password(int l) {
+  private int longitud;
+  private String contraseña;
+   public Password(int l) {
 	 this.longitud=l; 
-  }
+   }
+   
+   public void setContraseña(String c) {
+	   this.contraseña=c;
+   }
+   public int getContraseña() {
+	   return contraseña; 
+   }
+   public void setLongitud(int l) {
+	   this.longitud=l;
+   }
+   public int getLongitud() {
+	   return longitud;
+   }
+   
+   public boolean esFuerte() {
+	   return;
+   }
+   
+   public void generarPassword() {
+	   
+   }
 	public static void main(String[] args) {
 		Password p=new Password();
 	}
 
 }
+
