a53d9c55b1e539625e3463f54ee5b0b47e81f295
Paula Ortiz <sarkofy@hotmail.com>
2019-06-24 15:56:42 -0500

    Primera aproximación método mDeM

--- Password.java
+++ Password.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 
 public class Password {
   private String contraseña;
+  private char cadena;
   private static Scanner obDatos;
+  
   
    public void esFuerte() {
 	  obtenerContraseña();    
-	  if(contraseña.length()>5) {
+	  if(contraseña.length()>5 ) {
 		System.out.println("Es segura");  
 	  }else System.out.println("No lo es");
    }
@@ -16,10 +18,18 @@ public class Password {
 	   contraseña=obDatos.nextLine();
 	   return contraseña;
    }
+   public  void minusculasDeMayusculas() {
+	   obtenerContraseña(); 
+	   for(int i=0;i<contraseña.length();i++) {
+	   cadena=contraseña.charAt(i);
+	   System.out.println("El contenido es:"+ cadena);
+	    }
+   }
    
 	public static void main(String[] args) {
 		Password p=new Password (  );
 		obDatos = new Scanner(System.in);
-		p.esFuerte();	
+		p.esFuerte();
+		p.minusculasDeMayusculas();
 	}
 }
