431bbdaad5b91459a064a5accb539514a4da35cd
Paula Ortiz <sarkofy@hotmail.com>
2019-06-28 12:29:15 -0500

    Otra forma de implementar Regex NG

--- MasRegex.java
+++ MasRegex.java
@@ -1,34 +1,45 @@
 import java.util.regex.*;
 import java.util.Scanner;
 
+import java.util.regex.*;
+import java.util.Scanner;
 
 class MasRegex {
-//static String c="mi*";
-  static String c1,c2;
-//static String mi="michellemimimi";
-private static Scanner obDatos,obDatos2;
 
-public void Datos() {
+  static String c1,c2;
+  private static Scanner obDatos,obDatos2;
+  private static Matcher m;
+  private static Pattern p;
+
+  public String DatosAnalizar() {
 	System.out.println("Ingresa la palabra a analizar");
-	c1=obDatos.nextLine();
-	System.out.println("Ingresa el patróna  buscar");
-	c2=obDatos2.nextLine();	
+	c1=obDatos.next();
+	return c1;
+  }
+  public String DatosPatron(){
+  System.out.println("Ingresa el patróna  buscar");
+  c2=obDatos2.nextLine();	
+  return c2;
+ }
+  public void Patron(String c1,String c2) {
+	p=Pattern.compile(c1);
+    m=p.matcher(c2);
+	
+	while(m.find()) { 
+		System.out.println("Patrón encontrado");
+   }
 }
 
 	public static void main(String[] args) {
 		MasRegex r=new MasRegex();
 		obDatos= new Scanner(System.in);
 		obDatos2= new Scanner(System.in);
-		r.Datos();
-		//Pattern p=Pattern.compile("mi*"); //defino el patrón
-		Pattern p=Pattern.compile(c1); //entonces el atributo debe estar declarado como estático
-		Matcher m=p.matcher(c2);
-		while(m.find()) { //mientras encuentres el patrón
-			System.out.println("Patrón encontrado");
+		r.DatosAnalizar();
+		r.DatosPatron();
+		r.Patron(c1, c2);
 		
-		}
-		
-		
-	}
-
+	  }	
 }
+
+
+
