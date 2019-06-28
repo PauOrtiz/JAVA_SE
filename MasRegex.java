6c6f432f62e373efc97a6b66044e9435f7317a4c
Paula Ortiz <sarkofy@hotmail.com>
2019-06-28 16:05:53 -0500

    Corregido atributo del pattern y de matcher

--- MasRegex.java
+++ MasRegex.java
@@ -1,31 +1,29 @@
-import java.util.regex.*;
-import java.util.Scanner;
-
-import java.util.regex.*;
+port java.util.regex.*;
 import java.util.Scanner;
 
 class MasRegex {
-
   static String c1,c2;
-  private static Scanner obDatos,obDatos2;
-  private static Matcher m;
-  private static Pattern p;
+private static Scanner obDatos,obDatos2;
+private static Matcher m;
+private static Pattern p;
 
-  public String DatosAnalizar() {
+public String DatosAnalizar() {
 	System.out.println("Ingresa la palabra a analizar");
 	c1=obDatos.next();
 	return c1;
-  }
-  public String DatosPatron(){
-  System.out.println("Ingresa el patróna  buscar");
-  c2=obDatos2.nextLine();	
-  return c2;
- }
-  public void Patron(String c1,String c2) {
-	p=Pattern.compile(c1);
-    m=p.matcher(c2);
+}
+public String DatosPatron(){
+System.out.println("Ingresa el patróna  buscar");
+c2=obDatos2.nextLine();	
+return c2;
+}
+public void Patron(String c1,String c2) {
+	p=Pattern.compile(c2); //éste es el patrón
+    
+	System.out.println("compilado"); 
+	m=p.matcher(c1);//esta es la cadena que ingresa el usuario
 	
-	while(m.find()) { 
+	while(m.find()) { //mientras encuentres el patrón
 		System.out.println("Patrón encontrado");
    }
 }
@@ -37,9 +35,7 @@ class MasRegex {
 		r.DatosAnalizar();
 		r.DatosPatron();
 		r.Patron(c1, c2);
-		
 	  }	
 }
-
 
 
