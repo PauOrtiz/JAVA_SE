7989ea7a1ae4ac23d28b1f17dfddc53ff29af3d2
Paula Ortiz <sarkofy@hotmail.com>
2019-06-28 14:42:50 -0500

    Definición de queue

---
+++ Cola.java
@@ -0,0 +1,14 @@
+package ejercicios;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class Cola {
+	
+
+	public static void main(String[] args) {
+		Queue <String> c=new LinkedList<String>(); 
+       
+	}
+
+}
