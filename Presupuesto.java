f8db9c12078cdf60a707b7ab6ee221ccbb500238
Paula Ortiz <sarkofy@hotmail.com>
2019-07-02 10:04:25 -0500

    Stream acercamiento 1

---
+++ Presupuesto.java
@@ -0,0 +1,19 @@
+public class Presupuesto {
+   private int rh, sistemas, contabilidad; 
+   public Presupuesto(int rh,int sistemas,int contabilidad)
+   {
+	 this.rh=rh;
+	 this.sistemas=sistemas;
+	 this.contabilidad=contabilidad;
+   }
+   //TRATANDO COMO UN UN ARRAY
+   public void listarPresupuesto(){
+	 //conviertes los valores de los atributos de la clase en un arreglo
+	   ArrayList <Presupuesto> listadoPresupuesto=new ArrayList<Presupuesto>();
+   }
+	public static void main(String[] args) {
+		Presupuesto p=new Presupuesto(16500,56700,34567);
+
+	}
+
+}
