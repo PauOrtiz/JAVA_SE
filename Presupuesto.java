7adeeb0aeffae7d1b2b8ac0f448b69bd0d14ec7b
Paula Ortiz <sarkofy@hotmail.com>
2019-07-02 10:39:22 -0500

    Streams segundo acercamiento

--- Presupuesto.java
+++ Presupuesto.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Presupuesto {
    private int rh, sistemas, contabilidad; 
    public Presupuesto(int rh,int sistemas,int contabilidad)
@@ -9,11 +12,14 @@ public class Presupuesto {
    //TRATANDO COMO UN UN ARRAY
    public void listarPresupuesto(){
 	 //conviertes los valores de los atributos de la clase en un arreglo
-	   ArrayList <Presupuesto> listadoPresupuesto=new ArrayList<Presupuesto>();
+	ArrayList <Presupuesto> listadoPresupuesto=new ArrayList<Presupuesto>();
+	Stream<Presupuesto> r=listadoPresupuesto.stream()
+	.filter(listadoPresupuesto->listadoPresupuesto>16500);
+	System.out.println(r);
    }
 	public static void main(String[] args) {
 		Presupuesto p=new Presupuesto(16500,56700,34567);
-
+                    p.listarPresupuesto();
 	}
 
 }
