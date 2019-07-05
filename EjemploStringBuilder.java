87393991fa1eaa8eb3eec8a84208e6b3322dd9e5
Paula Ortiz <sarkofy@hotmail.com>
2019-07-05 11:22:45 -0500

    Se agregó replace

--- EjemploStringBuilder.java
+++ EjemploStringBuilder.java
@@ -1,8 +1,9 @@
 public class EjemploStringBuilder {
-	private String str= "Usando StringBuilder";
+
+	private String str= "Usando StringBuilder",ss;
 		int s, s2;
 	private StringBuilder str2= new StringBuilder(str);
-	private static Scanner obdatos;
+	private static Scanner obdatos,obd;
 	
 	public void longitud() {
 		System.out.println("La longitud de la cadena es:"+ str2.length());
@@ -13,15 +14,22 @@ public class EjemploStringBuilder {
 		str2.setLength(s);//realiza la operación sobre el argumento y ese es el nuevo valor para str2
 		System.out.println("La cadena es:" + str2);
 		}
+	public void reemplazar() {
+		System.out.println("Ingresa la cadena que ocupará el lugar de \"caracteres\" en la cadena:");
+		ss=obd.nextLine();	
+	    System.out.println("La nueva cadena es:"+ str.replace("caracteres", "ss") );
+	}   
 	public void imprimeConsola() {
 		System.out.println("El contenido de la cadena es:"+"******"+ str2+"*****");
 	}
+	
 	public static void main(String[] args) {
 	 EjemploStringBuilder b= new EjemploStringBuilder();
 	 obdatos=new Scanner(System.in);
 	 b.imprimeConsola();
 	 b.longitud();
 	 b.extraer();
+	 b.reemplazar();
 	 
 	}
 
