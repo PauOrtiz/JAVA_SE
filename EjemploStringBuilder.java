83066f5d20f5b72094ac931f9bda76769e359652
Paula Ortiz <sarkofy@hotmail.com>
2019-07-05 10:43:22 -0500

    Método longitud, extraer, imprimir en StringBuilder

--- EjemploStringBuilder.java
+++ EjemploStringBuilder.java
@@ -1,17 +1,27 @@
 public class EjemploStringBuilder {
-	String str= "Usando StringBuilder";
-	StringBuilder str2= new StringBuilder(str);
+	private String str= "Usando StringBuilder";
+		int s, s2;
+	private StringBuilder str2= new StringBuilder(str);
+	private static Scanner obdatos;
 	
 	public void longitud() {
 		System.out.println("La longitud de la cadena es:"+ str2.length());
 	}
+	public void extraer() {
+		System.out.println("Ingresa la cantidad de caracteres a leer:");
+		s=obdatos.nextInt();
+		str2.setLength(s);//realiza la operación sobre el argumento y ese es el nuevo valor para str2
+		System.out.println("La cadena es:" + str2);
+		}
 	public void imprimeConsola() {
 		System.out.println("El contenido de la cadena es:"+"******"+ str2+"*****");
 	}
 	public static void main(String[] args) {
 	 EjemploStringBuilder b= new EjemploStringBuilder();
+	 obdatos=new Scanner(System.in);
 	 b.imprimeConsola();
 	 b.longitud();
+	 b.extraer();
 	 
 	}
 
