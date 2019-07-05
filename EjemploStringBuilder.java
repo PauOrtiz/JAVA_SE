87ab9be8c75e980462bff461a66a8033dbbc7bf9
Paula Ortiz <sarkofy@hotmail.com>
2019-07-05 10:01:05 -0500

    Length()

--- EjemploStringBuilder.java
+++ EjemploStringBuilder.java
@@ -1,12 +1,18 @@
 public class EjemploStringBuilder {
 	String str= "Usando StringBuilder";
 	StringBuilder str2= new StringBuilder(str);
+	
+	public void longitud() {
+		System.out.println("La longitud de la cadena es:"+ str2.length());
+	}
 	public void imprimeConsola() {
 		System.out.println("El contenido de la cadena es:"+"******"+ str2+"*****");
 	}
 	public static void main(String[] args) {
 	 EjemploStringBuilder b= new EjemploStringBuilder();
 	 b.imprimeConsola();
+	 b.longitud();
+	 
 	}
 
 }
