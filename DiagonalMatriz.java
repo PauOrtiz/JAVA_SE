fec090cc81b2e95ea93b27d7e9c01fec62253471
Paula Ortiz <sarkofy@hotmail.com>
2019-07-17 15:06:35 -0500

    Inicializar

--- DiagonalMatriz.java
+++ DiagonalMatriz.java
@@ -1,13 +1,23 @@
 public class DiagonalMatriz {
   private static int[][] m;
-  
-  
+   
+  public void inicializar(int[][ ]pMatriz) {
+	 
+	        int aux = 1;
+	        for(int i=0;i<pMatriz.length;i++){
+	            for(int j=0;j<pMatriz[i].length;j++){
+	                pMatriz[i][j] = aux;
+	                aux++;
+	            }
+	        }
+	    }
+ 
 	public static void main(String[] args) {
 		m=new int[3][3];
         DiagonalMatriz matriz=new DiagonalMatriz();
-        matriz.vArray();
-        matriz.rellenar(); //se generan de forma aleatoria los elementos de la matriz
-        matriz.vMatriz();
+        matriz.inicializar(m); //se generan de forma aleatoria los elementos de la matriz
+        matriz.visualizarArray();
+        matriz.visualizarMatriz();
 	}
 
 }
