3a71aa183e9fb3c5afd90e2fed405cc359aaf1f9
Paula Ortiz <sarkofy@hotmail.com>
2019-07-17 15:13:42 -0500

    verMatriz()

--- DiagonalMatriz.java
+++ DiagonalMatriz.java
@@ -11,13 +11,25 @@ public class DiagonalMatriz {
 	            }
 	        }
 	    }
+ public void verMatriz(int[][]pMatriz) {
+	for(int i=0;i<pMatriz.length;i++){
+         for(int j=0;j<pMatriz[i].length;j++){
+             System.out.print("\t" + pMatriz[i][j]);
+         }
+         System.out.println();
+     }
+ }
+	 
  
+ public void verArray() {
+	 
+ }
 	public static void main(String[] args) {
 		m=new int[3][3];
         DiagonalMatriz matriz=new DiagonalMatriz();
         matriz.inicializar(m); //se generan de forma aleatoria los elementos de la matriz
-        matriz.visualizarArray();
-        matriz.visualizarMatriz();
+        matriz.verArray();
+        matriz.verMatriz(m);
 	}
 
 }
