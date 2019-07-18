02a8d757998a32b875c5492076a52fd1c8b2ca49
Paula Ortiz <sarkofy@hotmail.com>
2019-07-18 15:02:54 -0500

    botón

--- Libro.java
+++ Libro.java
@@ -9,16 +9,19 @@ import java.awt.Insets;
 import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 
-public class Libro extends JPanel {
-
-	/**
-	 * Create the panel.
-	 */
+public class Libro extends JPanel implements ActionListener{
+ 
+private JButton b;
+ private JLabel text;
+	
 	public Libro() {
 		setBackground(Color.MAGENTA);
 		setForeground(Color.BLACK);
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		
+		setLayout(null);
+		b=new JButton("Ingresar");
+		b.setBounds(100,150,100,30);
+		b.addActionListener(this);
+		add(b);
 		JLabel lblLibreraPegaso = new JLabel("LIBRER\u00CDA PEGASO");
 		lblLibreraPegaso.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblLibreraPegaso.setForeground(Color.BLACK);
