03caf543bd6e8204fafdd1172575abea40b9cae9
Paula Ortiz <sarkofy@hotmail.com>
2019-09-18 15:50:36 -0500

    Bloque estático

--- Paralelogramo.java
+++ Paralelogramo.java
@@ -1,22 +1,26 @@
 
 public class Paralelogramo { 
-	public static int b,h,a;
+	public static int b=0,h=0;
+	public static boolean flag=false;
 	
 	static {
-		b=10;
-		h=15;	
-	}
-	public static void area(int b, int h) {
+		Scanner s=new Scanner(System.in);
+		b= s.nextInt();
+		h=s.nextInt();
 		if(b>0 && h>0) {
-			System.out.println("el área es"+b*h);
-			
+			flag=true;
+				
 		}else{
 			 System.out.println("java.lang.Exception: Breadth and height must be positive");
-		 } 		
+		 } 
 	}
+	
 	public static void main(String[] args) {
 		
-		Paralelogramo.area(b,h);
+		if(flag) {
+			int a=b*h;
+			System.out.println(a);
+		}
 	}
 
 }
